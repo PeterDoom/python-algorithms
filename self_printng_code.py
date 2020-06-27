776b798be106ea1b49bcf29f19938f7c82f70163
PeterDoom <nanev0205@gmail.com>
2020-06-28 00:06:38 +0300

    First commit - Self printing code

---
+++ self_printng_code.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+with open("self_printng_code.py", "r") as file:
+    print(file.read())
+    file.close()
+
+with open("self_printng_code.py", "a") as file:
+    code = '''print("this is generated") \n'''
+    file.write(code)
+    file.close()
+print("this is generated") 
