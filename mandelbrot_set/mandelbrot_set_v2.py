4fa1c6638a8bff2d8c7e9516f296ce81841df257
PeterDoom <nanev0205@gmail.com>
2020-07-24 22:39:37 +0300

    Stealing a better multithreaded solution

--- mandelbrot_set/mandelbrot_set_v2.py
+++ mandelbrot_set/mandelbrot_set_v2.py
@@ -1,41 +1,57 @@
+import threading
 from PIL import Image
-from numpy import complex, array
-import colorsys
-import argparse
-import progressbar
-from multiprocessing import Pool
 
-WIDTH = 500
-IMAGE_ARRAY = []
-
-img = Image.new('RGB', (WIDTH, int(WIDTH / 2)))
-pixels = img.load()
-p = Pool(processes=10)
-
-
-def calculate_set(x, y):
-    c0 = complex(x, y)
-    c = 0
-    for i in range(1, 500):
-        if abs(c) > 2:
-            return create_pxl(i)
-        c = c * c + c0
-    return 0, 0, 0
+w = 4000
+h = 4000
+image = Image.new("RGB", (w, h))
+wh = w * h
+maxIt = 500  # max number of iterations allowed
+# drawing region (xa < xb & ya < yb)
+xa = -2.0
+xb = 1.0
+ya = -1.5
+yb = 1.5
+xd = xb - xa
+yd = yb - ya
+numThr = 100
 
 
-def create_pxl(i):
-    color = 255 * array(colorsys.hsv_to_rgb(i / 255.0, 1.0, 0.5))
-    return tuple(color.astype(int))
+class ManFrThread(threading.Thread):
+    def __init__(self, k):
+        self.k = k
+        threading.Thread.__init__(self)
+
+    def run(self):
+        # each thread only calculates its own share of pixels
+        for i in range(k, wh, numThr):
+            kx = i % w
+            ky = int(i / w)
+            a = xa + xd * kx / (w - 1.0)
+            b = ya + yd * ky / (h - 1.0)
+            x = a
+            y = b
+            for kc in range(maxIt):
+                x0 = x * x - y * y + a
+                y = 2.0 * x * y + b
+                x = x0
+                if x * x + y * y > 4:
+                    # various color palettes can be created here
+                    red = (kc % 8) * 32
+                    green = (16 - kc % 16) * 16
+                    blue = (kc % 16) * 16
+                    # lock.acquire()
+                    global image
+                    image.putpixel((kx, ky), (red, green, blue))
+                    # lock.release()
+                    break
 
 
-def build_image(image_size_x, image_size_y):
-    for x in range(image_size_x):
-        pixels[x, image_size_y] = calculate_set((x - (0.75 * WIDTH)) / (WIDTH / 4),
-                                                (image_size_y - (WIDTH / 4)) / (WIDTH / 4))
-
-
-for height in range(img.size[1]):
-    IMAGE_ARRAY.append(height)
-
-result = p.map(build_image())
-
+if __name__ == "__main__":
+    tArr = []
+    for k in range(numThr):  # create all threads
+        tArr.append(ManFrThread(k))
+    for k in range(numThr):  # start all threads
+        tArr[k].start()
+    for k in range(numThr):  # wait until all threads finished
+        tArr[k].join()
+    image.save("MandelbrotFractal.png", "PNG")
